626f2295a98c5827ec61376fee32ed4d09fe3928
Oleg Kulynych <kulinich0101@outlook.com>
2022-07-22 13:58:05 +0300

    AddAutomapper

---
+++ BookShop.Api/Profiles/Profiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BookShop.Api.Models;
+using BookShop.DTO.DTO;
+
+namespace BookShop.Api.Profiles
+{
+    public class Profiles: Profile
+    {
+        public Profiles()
+        {
+            CreateMap<BookCategory, BookCategoryDTO>()
+                .ForMember(
+                dest => dest.Id,
+                opt => opt.MapFrom(src => src.Id))
+                .ForMember(
+                dest => dest.Name,
+                opt => opt.MapFrom(src => src.Name)).ReverseMap();
+        }
+
+    }
+}
--- BookShop.Api/Repositories/Services/BookCategoryService.cs
+++ BookShop.Api/Repositories/Services/BookCategoryService.cs
@@ -1,4 +1,5 @@
-﻿using BookShop.Api.Models;
+﻿using AutoMapper;
+using BookShop.Api.Models;
 using BookShop.Api.Repositories.Interfaces;
 using BookShop.Shared.DTO;
 
@@ -8,9 +9,11 @@ namespace BookShop.Api.Repositories.Services
     {
 
         private readonly IBookCategoryRepository _bookCategoryRepository;
-        public BookCategoryService(IBookCategoryRepository bookCategoryRepository)
+        private readonly IMapper _mapper; 
+        public BookCategoryService(IBookCategoryRepository bookCategoryRepository, IMapper mapper)
         {
             _bookCategoryRepository = bookCategoryRepository;
+            _mapper = mapper;
         }
 
         public async Task AddBookCategoryAsync(BookCategoryDto bookCategoryDto)
@@ -22,26 +25,17 @@ namespace BookShop.Api.Repositories.Services
         public async Task<BookCategoryDto> GetBookCategoryByIdAsync(int id)
         {
             var bookCategory = await _bookCategoryRepository.GetCategoryByIdAsync(id);
-            return new BookCategoryDto { Id = bookCategory.Id, Name = bookCategory.Name };
+            var category = _mapper.Map<BookCategoryDTO>(bookCategory);
+            return category;
 
         }
 
         public async Task<IEnumerable<BookCategoryDto>> GetBookCategoriesAsync()
         {
             var bookCategories = await _bookCategoryRepository.GetCategoriesAsync();
+            var bookCategoryDTOs = _mapper.Map<List<BookCategoryDTO>>(bookCategories);
 
-            List<BookCategoryDto> bookCategoryDtos = new List<BookCategoryDto>();
-
-            foreach (BookCategory bookCategory in bookCategories)
-            {
-                var category = new BookCategoryDto
-                {
-                    Id = bookCategory.Id,
-                    Name = bookCategory.Name
-                };
-                bookCategoryDtos.Add(category);
-            }
-            return bookCategoryDtos;
+            return bookCategoryDTOs;
         }
 
         public async Task UpdateBookCategoryAsync(BookCategoryDto bookCategoryDto)
